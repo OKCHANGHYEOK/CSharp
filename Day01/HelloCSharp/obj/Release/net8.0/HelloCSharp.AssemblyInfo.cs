84fe362df33e6ecddb9692d3ebe9ff66ab4e593b
OCH <dkdldy2@naver.com>
2024-12-11 19:34:26 +0900

    12/11 C# 공부 2일차 커밋 - 남은 파일

--- Day01/HelloCSharp/obj/Release/net8.0/HelloCSharp.AssemblyInfo.cs
+++ Day01/HelloCSharp/obj/Release/net8.0/HelloCSharp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloCSharp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c2d4577e66cd518c8690902e13322de131e95b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloCSharp")]

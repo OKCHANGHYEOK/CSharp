ed30a6f9d9270641e65150b8293f37864ceae636
OCH <dkdldy2@naver.com>
2024-12-11 19:33:57 +0900

    12/11 C# 공부 2일차 커밋

---
+++ Day02/HelloCSharpWin/Caculator.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace HelloCSharpWin
+{
+    partial class Caculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            HelloLabel = new Label();
+            SuspendLayout();
+            // 
+            // HelloLabel
+            // 
+            HelloLabel.AutoSize = true;
+            HelloLabel.Location = new Point(61, 48);
+            HelloLabel.Name = "HelloLabel";
+            HelloLabel.Size = new Size(60, 25);
+            HelloLabel.TabIndex = 0;
+            HelloLabel.Text = "여기를 클릭하세요";
+            HelloLabel.BackColor = Color.Gray;
+            HelloLabel.Name = "HelloLabel";
+            HelloLabel.Click += HelloLabel_Click;
+            // 
+            // Caculator
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(HelloLabel);
+            Name = "Caculator";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+
+        #endregion
+
+        private Label HelloLabel;
+    }
+}
